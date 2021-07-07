d1885833c39b2632b94cddef7eec60cde8ff30ea
zF-9 <kewlzewl.kz@gmail.com>
2021-07-07 10:53:43 +0800

    added UI/UX design to login page (prototype)

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   HOST: "localhost",
-  USER: "test_dev",
-  PASSWORD: "qwerty123wasd",
+  USER: "root",
+  PASSWORD: "",
   DB: "AuthPrototype",
   dialect: "mysql",
   pool: {
--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var corsOptions = {
   origin: "http://localhost:8081"
 };
 
+app.use(express.static(__dirname + '/public'));
+
 app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
@@ -32,12 +34,17 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to Auth-Proto application." });
 });
 
+// Test route for ui/ux 
+app.get('/login', function (req, res) {
+  res.sendFile(__dirname + '/app/views/login.html');
+});
+
 // routes
 require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8484;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
